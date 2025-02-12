98d9dfdb75c353df699454cc507d886910ea09e6
lexsilva1 <jagmsilva@student.dei.uc.pt>
2025-02-12 15:54:10 +0000

    last commit

--- mealplaner/src/components/Mealplan.jsx
+++ mealplaner/src/components/Mealplan.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container, Grid, Card, CardHeader, CardContent, Typography } from '@mui/material';
+import { Container, Grid2 as Grid, Card, CardHeader, CardContent, Typography } from '@mui/material';
 import MealCard from './Mealcard';
 import './Mealplan.css';
 
@@ -44,47 +44,47 @@ const Mealplan = ({ mealPlanData }) => {
 
     const mealsByDayAndMealOfDay = groupMealsByDayAndMealOfDay(mealPlanData.mealPlans);
 
-    
+
     return (
         <Container className="meal-plan-container">
-            <Typography variant="h4" className="my-4 text-center">
-                Meal Plan for the Week
-            </Typography>
-            <Grid container spacing={2} wrap="nowrap" className="days-row">
-                {daysOfWeek.map((day, index) => (
-                    <Grid item key={index} className="day-column">
-                        <Card className="h-100">
-                            <CardHeader
-                                title={
-                                    <Typography variant="h5" component="h5" className="text-center">
-                                        {day}
-                                    </Typography>
-                                }
-                            />
-                            <CardContent className="card-content">
-                                {mealsByDayAndMealOfDay[day.toUpperCase()] ? (
-                                    Object.keys(mealsByDayAndMealOfDay[day.toUpperCase()]).map(
-                                        (mealOfDay, mealIndex) => (
-                                            <MealCard
-                                                key={`${index}-${mealIndex}`}
-                                                mealOfDay={mealOfDay}
-                                                mealPlans={
-                                                    mealsByDayAndMealOfDay[day.toUpperCase()][mealOfDay]
-                                                }
-                                            />
-                                        )
-                                    )
-                                ) : (
-                                    <Typography variant="body2" className="no-meals">
-                                        No meals available
-                                    </Typography>
-                                )}
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+<Grid container spacing={2} className="days-row">
+    {daysOfWeek.map((day, index) => (
+        <Grid
+            item
+            key={index}
+            xs={12} sm={6} md={4} lg={3}
+            className="day-column"
+        >
+            <Card className="h-100">
+                <CardHeader
+                    title={
+                        <Typography variant="h5" component="h5" className="text-center">
+                            {day}
+                        </Typography>
+                    }
+                />
+                <CardContent className="card-content">
+                    {mealsByDayAndMealOfDay[day.toUpperCase()] ? (
+                        Object.keys(mealsByDayAndMealOfDay[day.toUpperCase()]).map(
+                            (mealOfDay, mealIndex) => (
+                                <MealCard
+                                    key={`${index}-${mealIndex}`}
+                                    mealOfDay={mealOfDay}
+                                    mealPlans={mealsByDayAndMealOfDay[day.toUpperCase()][mealOfDay]}
+                                />
+                            )
+                        )
+                    ) : (
+                        <Typography variant="body2" className="no-meals">
+                            No meals available
+                        </Typography>
+                    )}
+                </CardContent>
+            </Card>
+        </Grid>
+    ))}
+</Grid>
+</Container>
     );
 
 };
